17abe6b468b090360d94fb0d7c6bd95ac4e0f65a
ANTONIO JEFFERSON <akjeff025@gmail.com>
2019-02-01 14:41:15 -0600

    initial commit getting user input

---
+++ src/script.js
@@ -0,0 +1,24 @@
+// code to add eventlistener to button and grab values from fields
+document.querySelector("#saveButton").addEventListener("click", event => {
+    const creatorUserInput = document.querySelector("#creatorInput").value
+    const colorUserInput = document.querySelector("#colorInput").value
+    const shapeUserInput = document.querySelector("#shapeInput").value
+    const creationUserInput = document.querySelector("#creationInput").value
+
+
+    // assign keys and values to object
+    const legoToSave = {
+        "creator": creatorUserInput,
+        "color": colorUserInput,
+        "shape": shapeUserInput,
+        "creation": creationUserInput
+    }
+  //post object to database.json
+    fetch("http://localhost:3000/legos", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(legoToSave)
+})
+})
