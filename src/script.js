a7d56acae6f5b893bdd974566f479d8ef0e9baa8
ANTONIO JEFFERSON <akjeff025@gmail.com>
2019-02-03 18:37:11 -0600

    get information from user practice complete

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 // code to add eventlistener to button and grab values from fields
 document.querySelector("#saveButton").addEventListener("click", event => {
     const creatorUserInput = document.querySelector("#creatorInput").value
-    const colorUserInput = document.querySelector("#colorInput").value
+    const colorUserInput = document.querySelector("#colorLabel").value
     const shapeUserInput = document.querySelector("#shapeInput").value
     const creationUserInput = document.querySelector("#creationInput").value
 
